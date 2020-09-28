b576d663ec4c58ef411608aba049a6ee7acdf35b
donghoon-khan <mustwin@kakao.com>
2020-09-28 21:33:18 +0900

    feature: Create ant-app

---
+++ ant-media-server/ant-app/src/test/java/com/khan/antapp/AntAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khan.antapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
